103de93c294dd8145c631bcfda912f39fd0e4eba
Rayan Castro <rayancdc@gmail.com>
2018-07-24 17:20:29 -0300

    Done wiki challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+new_article = Article.new(title: 'The meaning of life', content: "It's 42!")
+new_article.save
